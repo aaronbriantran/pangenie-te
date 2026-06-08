c3c908289f2f6d80dcd5f2410b00f7a85cdde325
aaronbtran_local <aaron.brian.tran@gmail.com>
2026-06-08 12:02:08 -0500

    lab notebook, new fastqc script created, and keeping open a result directory since fastqc needs that

---
+++ 1_fastqc/scripts/fastqc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#author Aaron Tran
+#date 8 June 2026
+#use This script runs fastqc on a given fasta file
+#input 
+
+#the sbatch variables were determined from:
+#https://rcs.ucalgary.ca/Bioinformatics_applications
+
+ml fastqc/0.12.1
+
