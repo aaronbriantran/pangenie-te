a9b3712d598b83b9dc54812d1f8464e8f4c522d4
aaronbtran_local <aaron.brian.tran@gmail.com>
2026-06-08 12:15:59 -0500

    finished the fastqc script as an example

--- 1_fastqc/scripts/fastqc.sh
+++ 1_fastqc/scripts/fastqc.sh
@@ -3,10 +3,18 @@
 #author Aaron Tran
 #date 8 June 2026
 #use This script runs fastqc on a given fasta file
-#input 
-
-#the sbatch variables were determined from:
-#https://rcs.ucalgary.ca/Bioinformatics_applications
+#input $1 is a given fastq file (so it does not measure pairs of files)
+#output an unzipped report, given as $2
+#args $3 is the log directory
+#compute 10 cores, 1400 Mb, maybe 5-10 minutes per file?, taken from https://rcs.ucalgary.ca/Bioinformatics_applications
 
 ml fastqc/0.12.1
 
+#runs the fastqc
+# -o takes output directory, --extract unzips output reports, --delete removes zipped reports after, file taken as an argument
+fastqc \
+   -o "$2" \
+   --extract \
+   --delete \
+   --threads number of threads \
+   "$1" &> "$3"
