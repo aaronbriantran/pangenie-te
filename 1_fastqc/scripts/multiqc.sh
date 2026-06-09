0d5c2ac7de14a16401853d2b3e163d082a86af56
aaronbtran_local <aaron.brian.tran@gmail.com>
2026-06-09 11:55:45 -0500

    documenting changes in the lab notebook, changing logging behavior for fastqc.sh to append, and writing stderr and stdout for multiqc

--- 1_fastqc/scripts/multiqc.sh
+++ 1_fastqc/scripts/multiqc.sh
@@ -6,17 +6,22 @@
 #input $1 is the results directory of the FastQC reports
 #output $2 is the output directory for where MultiQC should put its reports
 #args $3 is the environment yaml file for the conda environment that wraps multiqc, $4 is the log directory
-#compute ???
+#compute 10 cores, 1400 Mb, maybe 5-10 minutes per file? inherited directly from fastqc.sh
 
+#logic for creating conda environments if they aren't in your environment list
 if conda info --envs | grep -q multiqc; then
    conda create --file "$3"
 fi
 
 conda activate multiqc
 
+#run multiqc
+#-n is the name of the report, -o is the output directory (in results), -m is the module
+#for multiqc (inherited from Kara), -v forces verbose reporting
+#takes the directory of FastQC data as the argument, and passes stderr and stdout to the log file
 multiqc \
    -n report \
    -o "$2" \
    -m fastqc \
    -v \
-   "$1" > "$4"/multiqc.log
+   "$1" &> "$4"/multiqc.log
