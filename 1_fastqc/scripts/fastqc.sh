f47a46186036645a9e7bf15b34e44575d39f47d0
aaronbtran_local <aaron.brian.tran@gmail.com>
2026-06-09 16:02:43 -0500

    writing better explanations for Snakefiles in my lab notebook and in the middle of the first Snakefile for this step

--- 1_fastqc/scripts/fastqc.sh
+++ 1_fastqc/scripts/fastqc.sh
@@ -4,7 +4,7 @@
 #date 8 June 2026
 #use This script runs fastqc on a given fasta file
 #input $1 is a given fastq file (so it does not measure pairs of files)
-#output an unzipped report, given as $2
+#output an unzipped report; its output directory is given as $2
 #args $3 is the log directory
 #compute 10 cores, 1400 Mb, maybe 5-10 minutes per file?, taken from https://rcs.ucalgary.ca/Bioinformatics_applications
 
