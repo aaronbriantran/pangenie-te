0a9d7353cbe22bcd7c8f42636f2fdc5f854f4e31
aaronbtran_local <aaron.brian.tran@gmail.com>
2026-07-10 15:45:40 -0500

    minor pathing changes

--- 6_breakpoint/scripts/create_sample_vcfs.sh
+++ 6_breakpoint/scripts/create_sample_vcfs.sh
@@ -3,7 +3,7 @@ ml samtools
 
 mkdir -p results/create_sample_vcfs
 
-bcftools +split ../ -Oz -o results/create_sample_vcfs
+bcftools +split $1 -Oz -o results/create_sample_vcfs
 
 for sample_vcf in results/create_sample_vcfs/*; do
    tabix -p vcf "${sample_vcf}"
