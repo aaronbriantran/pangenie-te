8ce6c66800a22c8b0c50a23326057aa943fac15c
Aaron Tran <atran@wangcluster.g.wustl.edu>
2026-08-03 18:30:52 -0500

    mapping reads to the genotyped graph and packing and genotyping

--- 6_breakpoint/scripts/map_reads.sh
+++ 6_breakpoint/scripts/map_reads.sh
@@ -5,23 +5,23 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=32
 #SBATCH --time=1:30:00
-#SBATCH --output=logs/map_reads_gam.log
-#SBATCH --error=logs/map_reads_gam.err
+#SBATCH --output=logs/map_reads_gtype.log
+#SBATCH --error=logs/map_reads_gtype.err
 #SBATCH --mem=60G
 
 read1="/scratch/atran/final/1_reads/wgs/F1_1_WGS_MGI_L001_R1.fastq"
 read2="/scratch/atran/final/1_reads/wgs/F1_1_WGS_MGI_L001_R2.fastq"
-index_prefix="results/create_diploid_graphs/${1}/${1}"
+index_prefix="results/manual_diploid_graphs/${1}/${1}"
 
 #TODO fix pathing to inputs, wait for other scripts first
 
-mkdir -p results/map_reads
+mkdir -p results/map_reads_gtyped
 
 vg giraffe \
    --progress \
    -t "${SLURM_CPUS_PER_TASK}" \
    -d "${index_prefix}.dist" \
-   -Z "${index_prefix}.giraffe.gbz" \
-   -m "${index_prefix}.min" \
+   -Z "${index_prefix}.gbz" \
+   -m "${index_prefix}.withzip.min" \
    -f "${read1}" -f "${read2}" \
-   -o GAM -b default > "results/map_reads/${1}.gam"
+   -o GAM -b default > "results/map_reads_gtyped/${1}.gam"
--- 6_breakpoint/scripts/pack_and_gtype.sh
+++ 6_breakpoint/scripts/pack_and_gtype.sh
@@ -5,9 +5,10 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
 #SBATCH --time=01:00:00
-#SBATCH --output=logs/pack_and_call.log
-#SBATCH --error=logs/pack_and_call.err
+#SBATCH --output=logs/pack_and_gtype.log
+#SBATCH --error=logs/pack_and_gtype.err
 #SBATCH --mem=200G
+
 in_dir=results/${2}/${1}
 out_dir=results/pack_and_gtype/${1}
 
@@ -20,8 +21,8 @@ done
 
 # Compute the read support (use -a instead of -g for .gaf.gz input)
 # right mapping quality filter?
-vg pack -t "${SLURM_CPUS_PER_TASK}" -x "${in_dir}/${1}.gbz" -g results/map_reads/F1.gam -o "${out_dir}/${1}.pack" -Q 5
+vg pack -t "${SLURM_CPUS_PER_TASK}" -x "${in_dir}/${1}.xg" -g results/map_reads_gtyped/F1.gam -o "${out_dir}/${1}.pack" -Q 5
 
 # Genotype the graph (add -a to genotype all sites including 0/0)
 # The -z option restricts possible alleles to haplotypes in the GBZ which is usually faster and more accurate but only applies to GBZ input
-vg call -t "${SLURM_CPUS_PER_TASK}" "${in_dir}/${1}.giraffe.gbz" -k "${out_dir}/${1}.pack" -s "${1}" "${VCF_FLAGS[@]}" > "${out_dir}/${1}.vcf"
+vg call -t "${SLURM_CPUS_PER_TASK}" "${in_dir}/${1}.xg" -k "${out_dir}/${1}.pack" -s "${1}" "${VCF_FLAGS[@]}" > "${out_dir}/${1}.vcf"
